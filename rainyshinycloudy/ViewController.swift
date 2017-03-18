e8a37e3716b277a64980412a3c14c0db12bec0ac
Ravi Tiwari <rtiwari@digite.com>
2017-03-19 01:06:31 +0530

    Initial Commit

---
+++ rainyshinycloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by Ravi Tiwari on 3/19/17.
+//  Copyright © 2017 SelfStudy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
